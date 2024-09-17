b9a130acbf274e6f7beee913cb22ade86bc5f05c
im-bipo <bipinkhatri.ram@gmail.com>
2024-09-17 18:48:47 +0545

    Mailing Port Changed

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,13 +1,13 @@
-'use server';
-const nodemailer = require('nodemailer');
+"use server";
+const nodemailer = require("nodemailer");
 const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
 const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
 const SITE_MAIL_RECIEVER = process.env.SITE_MAIL_RECIEVER;
 const transporter = nodemailer.createTransport({
   host: SMTP_SERVER_HOST,
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: SMTP_SERVER_USERNAME,
     pass: SMTP_SERVER_PASSWORD,
@@ -29,16 +29,18 @@ export async function sendMail({
 }) {
   try {
     const isVerified = await transporter.verify();
+    console.log("Transporter Verified:", isVerified);
   } catch (error) {
-    console.error('Something Went Wrong', SMTP_SERVER_USERNAME, SMTP_SERVER_PASSWORD, error);
+    console.error("Error during transporter verification", error);
     return;
   }
+
   const info = await transporter.sendMail({
     from: email,
     to: sendTo || SITE_MAIL_RECIEVER,
     subject: subject,
     text: text,
-    html: html ? html : '',
+    html: html ? html : "",
   });
   return info;
 }
